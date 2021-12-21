c95c6c10b0bdef99386423e494f6389ab30d4541
MontassarBar <montassarbarbouchi7@gmail.com>
2021-12-21 20:31:49 +0100

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ''
+    for i in range(0, len(str)):
+        print("{}".format(
+            chr(ord(str[i]) - 32) if ord(str[i]) >= 97 and ord(
+                str[i]) <= 123 else chr(ord(str[i]))), end="")
+    print()
