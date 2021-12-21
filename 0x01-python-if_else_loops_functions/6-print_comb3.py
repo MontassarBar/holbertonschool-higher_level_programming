c10c5edabd215e1f8c20d48c1c681a5333f1c564
MontassarBar <montassarbarbouchi7@gmail.com>
2021-12-21 18:44:38 +0100

    comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,6 @@
 for i in range(0, 9):
     for f in range(0, 10):
         if i < f and (i + f) < 17:
-            print("{}".format(i), end="")
-            print("{}, ".format(f), end="")
-        elif i < f and (i + f) == 17:
-            print("{}".format(i), end="")
-            print("{}".format(f))
+            print("{}{}, ".format(i, f), end="")
+        elif (i + f) == 17:
+            print("{}{}".format(i, f))
