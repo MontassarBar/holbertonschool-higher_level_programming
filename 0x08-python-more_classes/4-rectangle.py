b44c4bdcdf4975fdf6c97c48f8632c24333ef017
MontassarBar <montassarbarbouchi7@gmail.com>
2022-01-24 19:31:18 +0100

    Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -5,41 +5,43 @@
 class Rectangle:
     '''a simple Rectangle'''
     def __init__(self, width=0, height=0):
-        if not isinstance(height, int):
-            raise TypeError("width must be an integer")
-        if height < 0:
-            raise ValueError("width must be >= 0")
-        self.__height = height
-        if not isinstance(width, int):
-            raise TypeError("height must be an integer")
-        if width < 0:
-            raise ValueError("height must be >= 0")
-        self.__width = width
-    '''property'''
-    @property
-    def height(self):
-        return self.__height
-    '''setter'''
-    @height.setter
-    def height(self, value):
-        self.__height = value
-    '''property'''
+        self.height = height
+        self.width = width
+
     @property
     def width(self):
         return self.__width
-    '''setter'''
+
     @width.setter
     def width(self, value):
-        self.__width = value
-    '''area of a Rectangle'''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
     def area(self):
         return self.__height * self.__width
-    '''perimeter of a Rectangle'''
+
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__height * 2) + (self.__width * 2)
-    '''print a Rectangle'''
+
     def __str__(self):
         if self.__width != 0 and self.__height != 0:
             return (('#' * self.__width) + '\n') * (
@@ -47,7 +49,7 @@ class Rectangle:
         else:
             d = ""
             return d
-    '''__repr__'''
+
     def __repr__(self):
         return "Rectangle(" + str(self.__width) + ',' + str(
             self.__height) + ")"
