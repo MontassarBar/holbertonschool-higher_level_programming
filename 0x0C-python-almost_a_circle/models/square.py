77a03ebb4b1dadf3679a63f3dbdbfdac2f99260e
MontassarBar <montassarbarbouchi7@gmail.com>
2022-02-07 12:30:10 +0100

    Square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Square class'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    ''' super class with id, x, y, width and height'''
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __Str__(self):
+        '''return [Square] (<id>) <x>/<y> - <size>'''
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.height)
