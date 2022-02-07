e3a7c0cb14e1f15422846d6b686820067e84168b
MontassarBar <montassarbarbouchi7@gmail.com>
2022-02-07 12:50:12 +0100

    Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,5 +10,5 @@ class Square(Rectangle):
 
     def __Str__(self):
         '''return [Square] (<id>) <x>/<y> - <size>'''
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
-            self.id, self.x, self.y, self.height)
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
