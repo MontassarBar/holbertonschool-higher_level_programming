02928ebf2c016611a4e2654a0c730b455c09bda0
MontassarBar <montassarbarbouchi7@gmail.com>
2022-03-12 18:57:39 +0100

    My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,3 @@
 -- c
 -- c
-SELECT tv_genres.name AS name FROM tv_show_genres left JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_show_genres.show_id = 8 ORDER BY name;
+SELECT tv_genres.name AS name FROM tv_show_genres left JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_show_genres.show_id = 8 ORDER BY name ASC;
