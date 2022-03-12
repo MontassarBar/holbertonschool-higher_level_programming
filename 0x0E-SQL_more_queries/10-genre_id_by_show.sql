999957a579dd1ca6772d50bec7e7622b9256063e
MontassarBar <montassarbarbouchi7@gmail.com>
2022-03-12 17:41:58 +0100

    Genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,3 @@
+-- c
+-- c
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE genre_id <> 'NULL' ORDER BY title, genre_id;
