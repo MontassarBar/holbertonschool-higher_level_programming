fd3962c82cc2cfcd612431a59f141cd611a39cfd
MontassarBar <montassarbarbouchi7@gmail.com>
2022-03-12 18:36:30 +0100

    Number of shows by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,3 @@
+-- c
+-- c
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_show_genres, tv_genres WHERE tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC
