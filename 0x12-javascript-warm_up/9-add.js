6638be7778ef253cf881339752d6aff8f9fc7774
MontassarBar <montassarbarbouchi7@gmail.com>
2022-04-04 22:34:57 +0200

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@ function add (a, b) {
   const x = a + b;
   return x;
 }
-const { argv } = require('process');
+const process = require('process');
 const args = process.argv;
 const y = add(parseInt(args[2]), parseInt(args[3]));
 if (isNaN(argv[2]) || isNaN(argv[3])) {
