01fa5a9d8fda552dbab7f41813ee3b33aef5d357
MontassarBar <montassarbarbouchi7@gmail.com>
2022-04-09 02:26:48 +0200

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ from the database hbtn_0e_0_usa'''
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
-    db = MySQLdb.Connect(host="localhost", port=3307, user=argv[
+    db = MySQLdb.Connect(host="localhost", port=3306, user=argv[
             1], passwd=argv[2], db=argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM states WHERE name REGEXP '^[N]' ORDER BY id ASC")
