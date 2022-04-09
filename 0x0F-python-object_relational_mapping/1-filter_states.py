71474b7bcf07aa40a3000b6eb342f067335d94fe
MontassarBar <montassarbarbouchi7@gmail.com>
2022-04-09 16:42:52 +0200

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,8 +6,7 @@ from the database hbtn_0e_0_usa'''
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
-    db = MySQLdb.Connect(host="localhost", port=3306, user=argv[
-            1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.Connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM states WHERE name RLIKE '^[N]' ORDER BY id ASC")
     query_rows = c.fetchall()
