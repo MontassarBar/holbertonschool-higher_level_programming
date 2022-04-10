393c65fbf692214b59183d4e5b204a5d5e948f4c
MontassarBar <montassarbarbouchi7@gmail.com>
2022-04-10 22:43:50 +0200

    Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
             argv[1], argv[2], argv[3]))
     session = Session(engine)
-    for state in session.query(State).order_by(State.id).filter(State.name.like('%a%')):
+    for state in session.query(State).order_by(State.id).filter(
+            State.name.like('%a%')):
         if 'a' in state.name:
             session.delete(state)
     session.commit()
