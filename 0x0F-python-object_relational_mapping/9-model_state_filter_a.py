075f5b4ff9803b53069a15e70fc11a692d44406a
MontassarBar <montassarbarbouchi7@gmail.com>
2022-04-10 18:05:59 +0200

    Contains

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''lists all State objects that contain the
+ letter a from the database hbtn_0e_6_usa'''
+
+
+import sqlalchemy
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+if __name__ == "__main__":
+    engine = sqlalchemy.create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1], argv[2], argv[3]))
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        for x in range(0, len(state.name) - 1):
+            if state.name[x] == 'a' or state.name[x] == 'A':
+                print("{}: {}".format(state.id, state.name))
+    session.close()
