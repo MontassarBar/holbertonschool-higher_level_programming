f9968a18677581128e9b6d6c8625a70dded0be11
MontassarBar <montassarbarbouchi7@gmail.com>
2022-05-17 19:58:12 +0200

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const axios = require('axios').default;
+const process = require('process');
 let i = 0;
-axios.get('https://swapi-api.hbtn.io/api/films')
+axios.get(process.argv[2])
   .then(function (response) {
     for (let x = 0; x < response.data.results.length; x++) {
       for (let y = 0; y < response.data.results[x].characters.length; y++) {
