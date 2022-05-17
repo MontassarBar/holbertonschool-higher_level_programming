652339017ca4bd17073163eec6dc86f6b6e5d9c8
MontassarBar <montassarbarbouchi7@gmail.com>
2022-05-17 21:48:21 +0200

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,12 @@
-#!/usr/bin/node
 const axios = require('axios').default;
 const process = require('process');
 let i = 0;
+let id = 18
 axios.get(process.argv[2])
   .then(function (response) {
     for (let x = 0; x < response.data.results.length; x++) {
       for (let y = 0; y < response.data.results[x].characters.length; y++) {
-        axios.get(response.data.results[x].characters[y])
-          .then(function (re) {
-            if (re.data.name === 'Wedge Antilles') { i++; }
-          });
+        if (response.data.results[x].characters[y] === 'https://swapi-api.hbtn.io/api/people/18/') { i++; }
       }
     }
     console.log(i);
